87809266485a095a60f37ee18097f9bf8ecee60e
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-20 12:40:43 -0600

    Commit

--- ATM/A01zainuddm.java
+++ ATM/A01zainuddm.java
@@ -6,7 +6,24 @@ import java.util.Scanner;
 
 public class A01zainuddm {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-	}
+    String inputName;
+    int inputNumber;
+    Scanner in = new Scanner(System.in);
+    boolean isValid;
+
+    //User input (an integer between 0 and 10000)
+    do {
+      isValid = true;
+      System.out.println("Enter a number between 0 and 10000");
+      inputNumber = Integer.parseInt(in.nextLine());
+
+      if (inputNumber < 0 || inputNumber > 10000) {
+        isValid = false;
+        System.out.println("Invalid. Try again.");
+      }
+
+    } while (!isValid);
+  }
 }
